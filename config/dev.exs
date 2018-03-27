9b6f571065f550e85b88b85f7009bd5d99398f30
fantypants <8563615@gmail.com>
2018-03-27 13:37:41 -0700

    Foods insert

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :fitcrm, Fitcrm.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "seasoft",
+  password: "seasoft",
   database: "fitcrm_dev",
   hostname: "localhost",
   pool_size: 10
