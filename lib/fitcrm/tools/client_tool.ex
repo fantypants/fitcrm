e45dfa75303ee6d800b75082d1aafa058b624f4c
fantypants <8563615@gmail.com>
2018-04-05 18:27:55 -0700

    IR put in

--- lib/fitcrm/tools/client_tool.ex
+++ lib/fitcrm/tools/client_tool.ex
@@ -14,15 +14,24 @@ defmodule Fitcrm.Tools.ClientTool do
       age = params["age"] |> IO.inspect
       sex = params["sex"] |> IO.inspect
       cystic = params["cystic"] |> IO.inspect
+      ir = params["ir"] |> IO.inspect
       params_new = PhysicsTool.modifyQuestionResults(%{"sex" => sex, "height" => height, "weight" => weight, "activity" => activity, "age" => age, "cystic" => cystic}) |> IO.inspect
       bmr = PhysicsTool.calculate_tdee(params_new) |> IO.inspect
-      tdee = PhysicsTool.scaleActivity(bmr, params_new["activity"]) |> IO.inspect
+      tdee_original = PhysicsTool.scaleActivity(bmr, params_new["activity"]) |> IO.inspect
       case cystic do
         "Yes" ->
-          tdee = tdee - 600
+          cyst1 = 600
         "No" ->
-          tdee = tdee
+          cyst1 = 0
       end
+
+      case ir do
+        "Yes" ->
+          ir1 = 200
+        "No" ->
+          ir1 = 0
+      end
+      tdee = tdee_original - cyst1 - ir1
       changesetmap = PhysicsTool.compileResults(user, params_new, bmr, tdee) |> IO.inspect
       changesetmap
     end
