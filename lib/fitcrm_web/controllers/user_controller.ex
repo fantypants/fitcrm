aff766cd6c0cc5ecfdadcc203addd73b2d211589
fantypants <8563615@gmail.com>
2018-05-04 15:18:16 -0700

    age fields in

--- lib/fitcrm_web/controllers/user_controller.ex
+++ lib/fitcrm_web/controllers/user_controller.ex
@@ -59,11 +59,21 @@ defmodule FitcrmWeb.UserController do
   def newquestion(%Plug.Conn{assigns: %{current_user: user}} = conn, %{"id" => id}) do
     changeset = User.changeset(%User{}, %{name: "name"})
     user = (id == to_string(user.id) and user) || Accounts.get(id)
+    ages = [
+    "10", "11", "12","13", "14", "15", "16", "17", "18", "19",
+    "20", "21", "22","23", "24", "25", "26", "27", "28", "29",
+    "30", "31", "32","33", "34", "35", "36", "37", "38", "39",
+    "40", "41", "42","43", "44", "45", "46", "47", "48", "49",
+    "50", "51", "52","53", "54", "55", "56", "57", "58", "59",
+    "60", "61", "62","63", "64", "65", "66", "67", "68", "69",
+    "70", "71", "72","73", "74", "75", "76", "77", "78", "79",
+    "80", "81", "82","83", "84", "85", "86", "87", "88", "89",
+    "90", "91", "92","93", "94", "95", "96", "97", "98", "99"]
     types = Fitcrm.Repo.all(Workout) |> Enum.map(&(&1.type)) |> IO.inspect
     levels = Fitcrm.Repo.all(Workout) |> Enum.map(&(&1.level)) |> IO.inspect
     workouts = Fitcrm.Repo.all(Workout) |> Enum.map(fn(a) -> %{type: a.type, level: a.level} end)
     #params = %{"weight" => "0", "age" => "0", "height" => "0", "sex" => "Male", "activity" => "Sedentary", "cystic" => "No"}
-    render(conn, "questionform.html", changeset: changeset, user: user, types: types, levels: levels, workouts: workouts)
+    render(conn, "questionform.html", changeset: changeset, user: user, types: types, levels: levels, workouts: workouts, ages: ages)
   end
 
   def question(%Plug.Conn{assigns: %{current_user: user}} = conn, %{"id" => id, "user" => params}) do
